e3ac28815ff6afcd1564053ceef51698020ba75a
Artem_Teplygin <teplygin07@mail.ru>
2025-07-12 09:24:52 +0300

    add polymorphic Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,11 +1,13 @@
 import { ComponentPropsWithoutRef } from "react"
 import s from "./Button.module.css"
 import clsx from "clsx"
+import { Slot } from "@radix-ui/react-slot";
 
 type Props = {
   variant?: "primary" | "secondary" | "outline"
+  asChild?: boolean
 } & ComponentPropsWithoutRef<"button">
-export const Button = ({ variant = "primary", className, ...rest }: Props) => {
-  return <button className={clsx(s.button, s[variant], className)} {...rest} />
-
+export const Button = ({ variant = "primary", asChild, className, ...rest }: Props) => {
+  const Component = asChild ? Slot : "button"
+  return <Component className={clsx(s.button, s[variant], className)} {...rest} />
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,6 @@
 import { Cart } from "../Cart/Cart.tsx"
 import s from "./Header.module.css"
+import { Button } from "../Button/Button.tsx"
 
 export const Header = () => {
   return (
@@ -7,6 +8,9 @@ export const Header = () => {
       <div className={s.container}>
         <h3>logotype</h3>
         <Cart />
+        <Button asChild>
+          <a href="/sign-up">Sign Up</a>
+        </Button>
       </div>
     </div>
   )
